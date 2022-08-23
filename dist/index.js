90164cd47274d6e6727222197355da9201c3f1e5
Denis Nahuamel <nahuamel.denis@gmail.com>
2022-08-23 11:52:30 -0500

    add .env file

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+// initialize configuration
+dotenv_1.default.config();
+// port is now available to the Node.js runtime
+// as if it were an environment variable
+const port = process.env.SERVER_PORT;
 const app = (0, express_1.default)();
-const port = 8083; // default port to listen
 // define a route handler for the default home page
 app.get("/", (req, res) => {
     res.send("Hello world!");
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import express from "express";
-const app = express();
-const port = 8083; // default port to listen
+import dotenv from "dotenv";
 
+// initialize configuration
+dotenv.config();
+
+// port is now available to the Node.js runtime
+// as if it were an environment variable
+const port = process.env.SERVER_PORT;
+
+const app = express();
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
     res.send( "Hello world!" );
