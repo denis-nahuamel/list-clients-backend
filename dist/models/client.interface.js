df8ec70103ead25c5307bc322f34de0b2086c4b9
Denis Nahuamel <nahuamel.denis@gmail.com>
2022-08-25 10:29:24 -0500

    add types to req and res parameters

--- dist/models/client.interface.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var EEstado;
-(function (EEstado) {
-    EEstado[EEstado["ACTIVO"] = 0] = "ACTIVO";
-    EEstado[EEstado["INACTIVO"] = 1] = "INACTIVO";
-})(EEstado || (EEstado = {}));
-//# sourceMappingURL=client.interface.js.map
--- src/controllers/client.controller.ts
+++ src/controllers/client.controller.ts
@@ -1,8 +1,9 @@
 import { IClient } from "src/models/client.interface";
 import * as ClientService from '../services/client.service';
+import {Request, Response} from "express";
 
 // get all the clients
-export const getClientList = async (req:any, res:any) => {
+export const getClientList = async (req:Request, res:Response) => {
   const clientList: IClient[] = await ClientService.getAllClients();
   try {
     res.status(200).send(clientList);
